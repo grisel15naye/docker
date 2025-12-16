a9d2a3a5bb38ffac1b886590020806180a81f3fd
ramirezventuranayeli@gmail.com <ramirezventuranayeli@gmail.com>
2025-12-15 21:02:41 -0500

    first commit

---
+++ src/main/java/pe/edu/idat/demo_rest_bd/DemoRestBdApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.demo_rest_bd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRestBdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRestBdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/idat/demo_rest_bd/DemoRestBdApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.demo_rest_bd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRestBdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
